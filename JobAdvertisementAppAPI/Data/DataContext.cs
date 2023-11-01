55df8d3e52555708419017e717274d84186d18fb
123 <123>
2023-11-01 18:35:30 +0100

    Database migration

---
+++ JobAdvertisementAppAPI/Data/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using JobAdvertisementAppAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobAdvertisementAppAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Company> Company { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Education> Education { get; set; }
+        public DbSet<JobExperience> JobExperience { get; set; }
+        public DbSet<JobLevel> JobLevel { get; set; }
+        public DbSet<JobType> JobType { get; set; }
+        public DbSet<Language> Language { get; set; }
+        public DbSet<Offer> Offer { get; set; }
+        public DbSet<Profile> Profile { get; set; }
+        public DbSet<TypeOfContract> TypeOfContract { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserLanguage> UserLanguage { get; set; }
+        public DbSet<WorkingShift> WorkingShift { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserLanguage>().HasKey(ul => new { ul.UserId, ul.LanguageId });
+            modelBuilder.Entity<UserLanguage>().HasOne(u => u.User).WithMany(ul => ul.UserLanguages).HasForeignKey(u => u.UserId);
+            modelBuilder.Entity<UserLanguage>().HasOne(l => l.Language).WithMany(ul => ul.UserLanguages).HasForeignKey(l => l.LanguageId);
+        }
+    }
+}
---
+++ JobAdvertisementAppAPI/Migrations/20231101173042_InitialMigration.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobAdvertisementAppAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobLevel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Level = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobLevel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Language",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Level = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Language", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeOfContract",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeOfContract", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Occupation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CareerSummary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Skills = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkingShift",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Shift = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkingShift", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Organizer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CourseStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CourseEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Course_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Education",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Institution = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Town = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EducationLevel = table.Column<int>(type: "int", nullable: false),
+                    FieldOfStudy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PeriodOfEducationStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PeriodOfEducationEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Education", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Education_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobExperience",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PeriodOfEmploymentStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PeriodOdEmploymentEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Responsibilities = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobExperience", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobExperience_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profile",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profile_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLanguage",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    LanguageId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLanguage", x => new { x.UserId, x.LanguageId });
+                    table.ForeignKey(
+                        name: "FK_UserLanguage_Language_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Language",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserLanguage_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Offer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MinimumWege = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MaximumWege = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    WorkingDays = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkingHours = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Expires = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AvailablePosts = table.Column<int>(type: "int", nullable: false),
+                    Benefits = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Requirements = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Responsibilities = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyId = table.Column<int>(type: "int", nullable: true),
+                    JobLevelId = table.Column<int>(type: "int", nullable: true),
+                    TypeOfContractId = table.Column<int>(type: "int", nullable: true),
+                    JobTypeId = table.Column<int>(type: "int", nullable: true),
+                    WorkingShiftId = table.Column<int>(type: "int", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Offer_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Offer_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Offer_JobLevel_JobLevelId",
+                        column: x => x.JobLevelId,
+                        principalTable: "JobLevel",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Offer_JobType_JobTypeId",
+                        column: x => x.JobTypeId,
+                        principalTable: "JobType",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Offer_TypeOfContract_TypeOfContractId",
+                        column: x => x.TypeOfContractId,
+                        principalTable: "TypeOfContract",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Offer_WorkingShift_WorkingShiftId",
+                        column: x => x.WorkingShiftId,
+                        principalTable: "WorkingShift",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_UserId",
+                table: "Course",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Education_UserId",
+                table: "Education",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobExperience_UserId",
+                table: "JobExperience",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_CategoryId",
+                table: "Offer",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_CompanyId",
+                table: "Offer",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_JobLevelId",
+                table: "Offer",
+                column: "JobLevelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_JobTypeId",
+                table: "Offer",
+                column: "JobTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_TypeOfContractId",
+                table: "Offer",
+                column: "TypeOfContractId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_WorkingShiftId",
+                table: "Offer",
+                column: "WorkingShiftId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_UserId",
+                table: "Profile",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLanguage_LanguageId",
+                table: "UserLanguage",
+                column: "LanguageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropTable(
+                name: "Education");
+
+            migrationBuilder.DropTable(
+                name: "JobExperience");
+
+            migrationBuilder.DropTable(
+                name: "Offer");
+
+            migrationBuilder.DropTable(
+                name: "Profile");
+
+            migrationBuilder.DropTable(
+                name: "UserLanguage");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropTable(
+                name: "JobLevel");
+
+            migrationBuilder.DropTable(
+                name: "JobType");
+
+            migrationBuilder.DropTable(
+                name: "TypeOfContract");
+
+            migrationBuilder.DropTable(
+                name: "WorkingShift");
+
+            migrationBuilder.DropTable(
+                name: "Language");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
