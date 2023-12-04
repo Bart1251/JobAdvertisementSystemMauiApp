012a7d6cb7989c98fdd825a044fe2cfe857e1dbd
Bart1251 <bartlomiej.wiecek@vp.pl>
2023-12-04 18:22:43 +0100

    models mapping

--- JobAdvertisementAppAPI/Helper/MappingProfiles.cs
+++ JobAdvertisementAppAPI/Helper/MappingProfiles.cs
@@ -9,8 +9,32 @@ namespace JobAdvertisementAppAPI.Helper
     {
         public MappingProfiles()
         {
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+            CreateMap<Company, CompanyDto>();
+            CreateMap<CompanyDto, Company>();
+            CreateMap<Course, CourseDto>();
+            CreateMap<CourseDto, Course>();
+            CreateMap<Education, EducationDto>();
+            CreateMap<EducationDto, Education>();
+            CreateMap<JobExperience, JobExpirienceDto>();
+            CreateMap<JobExpirienceDto, JobExperience>();
+            CreateMap<JobLevel, JobLevelDto>();
+            CreateMap<JobLevelDto, JobLevel>();
+            CreateMap<JobType, JobTypeDto>();
+            CreateMap<JobTypeDto, JobType>();
+            CreateMap<Language, LanguageDto>();
+            CreateMap<LanguageDto, Language>();
+            CreateMap<Offer, OfferDto>();
+            CreateMap<OfferDto, Offer>();
+            CreateMap<Profile, ProfileDto>();
+            CreateMap<ProfileDto, Profile>();
+            CreateMap<TypeOfContract, TypeOfContractDto>();
+            CreateMap<TypeOfContractDto, TypeOfContract>();
             CreateMap<User, UserDto>();
             CreateMap<UserDto, User>();
+            CreateMap<WorkingShift, WorkingShiftDto>();
+            CreateMap<WorkingShiftDto, WorkingShift>();
         }
     }
 }
