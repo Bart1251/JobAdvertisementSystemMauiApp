d5490631c9b5f92277c53e68944c11916e3fcd6a
Bart1251 <bartlomiej.wiecek@vp.pl>
2023-12-05 19:48:45 +0100

    Database update - user

--- JobAdvertisementAppAPI/Dto/UserDto.cs
+++ JobAdvertisementAppAPI/Dto/UserDto.cs
@@ -6,6 +6,7 @@
         public string? FirstName { get; set; }
         public string? SecondName { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public string? Adress { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? PhoneNumber { get; set; }
---
+++ JobAdvertisementAppAPI/Migrations/20231205172301_User updated Adress.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobAdvertisementAppAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserupdatedAdress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Adress",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adress",
+                table: "User");
+        }
+    }
+}
